9d5ef983f522edcf4565a28c498704ef2ab536c7
github-action-benchmark <github@users.noreply.github.com>
2025-05-14 13:45:17 +0000

    add Benchmark (pytest) benchmark result for 9b1273b6269e272b465db41a0788565cf5f40fad

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1747230317789,
+  "repoUrl": "https://github.com/steelannelida/benchmarktest",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "steelannelida@Dmitrys-MacBook-Pro.local",
+            "name": "Dmitry Chernenkov"
+          },
+          "committer": {
+            "email": "steelannelida@Dmitrys-MacBook-Pro.local",
+            "name": "Dmitry Chernenkov"
+          },
+          "distinct": true,
+          "id": "9b1273b6269e272b465db41a0788565cf5f40fad",
+          "message": "fix permissions",
+          "timestamp": "2025-05-14T15:44:53+02:00",
+          "tree_id": "4b93815adc1d2f4108cf7bd2a2bc1010ba8ec275",
+          "url": "https://github.com/steelannelida/benchmarktest/commit/9b1273b6269e272b465db41a0788565cf5f40fad"
+        },
+        "date": 1747230317284,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test_all.py::test_my_case",
+            "value": 9.985709012460985,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002517991146028376",
+            "extra": "mean: 100.14311440000085 msec\nrounds: 10"
+          }
+        ]
+      }
+    ]
+  }
+}
